493e3ff6f65f778e9947bc4cc587d5664438a061
shinyleefeon <leecastro7@gmail.com>
2026-02-04 09:16:01 -0500

    Bot goes online, and checks for a simple command in any chat

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+
+func main() {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	token := os.Getenv("DiscordBotToken")
+	if token == "" {
+		fmt.Println("No Discord bot token provided in environment variable 'DiscordBotToken'")
+		return
+	}
+
+
+	//make new bot
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentMessageContent
+	//open a websocket connection to Discord
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection: ", err)
+		return
+	}
+	defer dg.Close()
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+
+	//wait for a signal to quit
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	<-sc
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	//ignore messages from the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "!clanker" {
+		s.ChannelMessageSend(m.ChannelID, "Beep boop I'm a bot")
+	}
+}
